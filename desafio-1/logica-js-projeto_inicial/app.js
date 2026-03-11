7a13771d668daa5c8fdc81691e2256b561538040
Vinícius  Fuentes Cavalcanti <142112837+vinifcavalcanti@users.noreply.github.com>
2026-03-11 12:19:12 -0300

    Update app.js

--- desafio-1/logica-js-projeto_inicial/app.js
+++ desafio-1/logica-js-projeto_inicial/app.js
@@ -4,5 +4,14 @@ let idade = 25;
 let numeroDeVendas = 50;
 let saldoDisponivel = 1000;
 
+
 let mensagemDeErro = 'Erro! Preencha todos os campos'
 alert(mensagemDeErro)
+let nomeU = prompt('Qual o seu nome?')
+let idadeU = prompt('Qual a sua idade?')
+
+if (idadeU >= 18){
+    alert('Você pode tirar habilitação!')
+}else{
+    alert('Você é muito novo!')
+}
