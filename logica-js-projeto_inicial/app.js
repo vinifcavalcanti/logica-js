0dfb80f5c36c8d655f1b3c24d7e9df11c3553c88
Vinícius  Fuentes Cavalcanti <142112837+vinifcavalcanti@users.noreply.github.com>
2026-03-11 11:53:44 -0300

    .

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,8 @@
-alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 5;
-let chute = prompt("Escolha um número entre 1 e 10");
+alert('Boas vindas ao nosso site!')
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
 
-if (chute == numeroSecreto){
-    console.log("Isso ai! Você descobriu o número secreto");
-}
+let mensagemDeErro = 'Erro! Preencha todos os campos'
+alert(mensagemDeErro)
