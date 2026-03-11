3c174d4007df3a92dcbc23d1b3702eac429b9d75
Vinícius  Fuentes Cavalcanti <142112837+vinifcavalcanti@users.noreply.github.com>
2026-03-11 16:22:12 -0300

    .

---
+++ desafios/desafio-2/logica-js-projeto_inicial/app.js
@@ -0,0 +1,9 @@
+let diaSemana = prompt('Qual é o dia da semana?')
+
+if (diaSemana == 'sábado'){
+    alert('Bom fim de semana!')
+}else if( diaSemana == 'Domingo'){
+    alert('Bom fim de semana')
+}else {
+    alert('Boa semana!')
+}
